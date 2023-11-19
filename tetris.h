22827351a75da685f0a67911550cb838d2da1c30
jpedrob <jpedrobacker@outlook.com>
2023-11-19 19:35:25 -0300

    My own tetris start

---
+++ tetris.h
@@ -0,0 +1,16 @@
+#ifndef TETRIS_H
+# define TETRIS_H
+
+
+# include <stdlib.h>
+# include <stdlib.h>
+# include <time.h>
+# include <sys/time.h>
+# include <ncurses.h>
+
+# define ROWS 20
+# define COLS 15
+# define TRUE 1
+# define FALSE 0
+
+#endif
