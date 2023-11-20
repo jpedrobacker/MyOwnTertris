6e81acc4acce61eb7a30f583508472f635ba1c75
jpedrob <jpedrobacker@outlook.com>
2023-11-19 22:19:04 -0300

    ??????

--- tetris.c
+++ tetris.c
@@ -1,5 +1,6 @@
 #include "tetris.h"
 
+char Table[ROWS][COLS] = {0};
 int	score = 0;
 char GameOn = TRUE;
 Shape current;
@@ -12,29 +13,6 @@ const Shape ShapesArr[7] =
     {(char *[]){(char []){1,0,0},(char []){1,1,1}, (char []){0,0,0}}, 3},      //ML_shape
     {(char *[]){(char []){1,1},(char []){1,1}}, 2},                            //SQ_shape
 };
-
-void	print_board()
-{
-	int row = 0;
-	int col = 0;
-
-	while (row <= ROWS)
-	{
-		while(col <= COLS)
-		{
-			if (col == 0 || col == COLS)
-				printw("|");
-			else if (row == ROWS)
-				printw("*");
-			else
-				printw(" ");
-			col++;
-		}
-		printw("\n");
-		row++;
-	}
-	printw("\nScore: %d\n", score);
-}
 
 Shape CopyShape(Shape shape)
 {
@@ -68,8 +46,72 @@ void	deleteshape(Shape shape)
 	free(shape.array);
 }
 
+void	RandomShape(Shape shape)
+{
+
+}
+
+void	print_board()
+{
+	char buffer[ROWS][COLS] = {0};
+	int i = 0;
+	int j = 0;
+
+	while(i < current.width)
+	{
+		while (j < current.width)
+		{
+			if (current.array[i][j])
+				buffer[current.row+i][current.col+j] = current.array[i][j];
+			j++;
+		}
+		i++;
+	}
+	clear();
+	i = 0;
+	while (i < COLS-9)
+	{
+		printw(" ");
+		i++;
+	}
+	i = 0;
+	j = 0;
+	while (i < ROWS)
+	{
+		while(j < COLS)
+		{
+			printw("%c ", (Table[i][j] + buffer[i][j] ? '#': '.'));
+			j++;
+		}
+		printw("\n");
+	}
+	printw("\nScore: %d\n", score);
+	/*while (row <= ROWS)
+	{
+		while(col <= COLS)
+		{
+			if (col == 0 || col == COLS)
+				printw("|");
+			else if (row == ROWS)
+				printw("*");
+			else
+				printw(" ");
+			col++;
+		}
+		printw("\n");
+		row++;
+	}
+	printw("\nScore: %d\n", score);*/
+}
+
 int main(void)
 {
+	score = 0;
+	int c;
+
+	initscr();
 	print_board();
+	endwin();
+
 	return (0);
 }
